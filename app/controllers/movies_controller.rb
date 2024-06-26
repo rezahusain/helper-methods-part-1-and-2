6a1175651a12410e214a55813fb0c26a1836d82b
rezahusain <rezahusain99@gmail.com>
2024-06-26 15:33:12 -0500

    Refactored in order to mass assign attributes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,9 +22,8 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
@@ -39,10 +38,8 @@ class MoviesController < ApplicationController
   end
 
   def update
-    movie = Movie.find(params.fetch(:id))
-
-    movie.title = params.fetch(:title)
-    movie.description = params.fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    movie = Movie.new(movie_attributes)
 
     if movie.valid?
       movie.save
