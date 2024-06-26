a7a3d32f40b98108e38756cc8d632bf75e762a2d
rezahusain <rezahusain99@gmail.com>
2024-06-26 11:17:26 -0500

    Refactored forms with helper methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,6 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new"
   end
 
   def index
@@ -15,9 +14,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index"
-      end
+      format.html
     end
   end
 
@@ -28,7 +25,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/show"
   end
 
   def create
@@ -40,7 +36,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new"
+      render "new"
     end
   end
 
@@ -51,7 +47,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/edit"
   end
 
   def update
@@ -63,9 +58,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to movie_url(@the_movie), notice: "Movie updated successfully."
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
     else
-      redirect_to movie_url(@the_movie), alert: "Movie failed to update successfully." 
+      redirect_to movie_url(the_movie), alert: "Movie failed to update successfully." 
     end
   end
 
